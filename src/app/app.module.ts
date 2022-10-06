cf748e00ce5da9c0d772f94546b36a8a4ce03585
= <masixolematolweni.1@gmail.com>
2022-10-07 01:30:39 +0200

    Finished off site layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { NavComponent } from './nav/nav.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    NavComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
